ab4279ed1655cfff24d932b4d7e2a0c24f697bf0
Vitalii Kotliarenko <vkotlyarenko@gmail.com>
2015-04-09 01:39:56 +0300

    experiments

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -1,33 +1,50 @@
 package org.example
 
 import _root_.kafka.serializer.StringDecoder
+import _root_.kafka.utils.Json
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.sql._
+import org.example.model.Event
 
-object Main extends App {
+object Main {
+  def main(args: Array[String]) = {
 
-	val sparkConf = new SparkConf().setAppName("streamingPoc")
-	val ssc = new StreamingContext(sparkConf, Seconds(1))
-	ssc.checkpoint("checkpoint")
+    val sparkConf = new SparkConf().setAppName("streamingPoc")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    //ssc.checkpoint("checkpoint")
 
-	val kafkaParams = Map(
-		"zookeeper.connect" -> "localhost:2181",
-		"group.id" -> "spark-streaming-poc",
-		"zookeeper.connection.timeout.ms" -> "1000")
+    val kafkaParams = Map(
+      "metadata.broker.list" -> "localhost:9092"
+    )
 
-	val dataset = ssc.sparkContext.emptyRDD[(String, String)]
 
-	val sqlContext = new SQLContext(ssc.sparkContext)
-	import sqlContext.implicits._
+    val sqlContext = new SQLContext(ssc.sparkContext)
 
-	val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, Set("test"))
-	stream.transform { batchRDD =>
-		batchRDD.join(dataset)
-	}
+    import sqlContext.implicits._
 
-	val dataFrame = sqlContext.createDataFrame(dataset)
-	stream.persist()
+    val dataset = ssc.sparkContext.makeRDD(Seq[(String, String)]())
+
+    val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, Set("test"))
+    stream.start()
+
+    /*stream.foreachRDD { rdd =>
+      println("Count = " + rdd.count())
+      println("Content: \n" + rdd.collectAsMap())
+    }*/
+    println("Streaming started")
+    stream.foreachRDD { batchRDD =>
+      println("In foreach RDD loop")
+      val united = dataset.union(batchRDD)
+      val jsonRDD = sqlContext.jsonRDD(dataset.map(e => e._2))
+      jsonRDD.registerTempTable("events")
+      val rows = sqlContext.sql("SELECT * FROM events").collect()
+      println("Query result: \n" + rows.map(r => r.mkString(", ")).mkString("\n"))
+    }
+
+    ssc.awaitTerminationOrTimeout(120000)
+    println("Streaming finished")
+  }
 
 }
